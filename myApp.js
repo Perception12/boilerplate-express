67c3383c533a4f2e77f052efa2f87e719af325dd
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-07-14 23:02:18 +0100

    use .env

--- myApp.js
+++ myApp.js
@@ -1,7 +1,21 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+    var response = "hello json";
+    if (process.env.MESSAGE_STYLE == 'uppercase'){
+        response = response.toUpperCase();
+    }
+
+    return res.json({"message": response});
+})
 
 
 
